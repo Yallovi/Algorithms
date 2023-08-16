a477587ad5f7e90f77558c417d4a7e75b4b298ff
Viktor Moskalenko <v.moskalenko@brightlab.me>
2023-08-16 22:19:54 +0300

    added task <contains duplicate>

---
+++ LeetCode/contains-duplicate.js
@@ -0,0 +1,41 @@
+/**
+ * Учитывая целочисленный массив nums, верните true, если какое-либо значение появляется в массиве по крайней мере дважды, и верните false, если каждый элемент различен.
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ *
+ */
+
+const nums = [1,2,3,1]
+
+/**
+ * First variant
+ * Time O(N) | Space O(N)
+ */
+function containsDuplicate(nums, setArray= new Set()) {
+
+    for (let i = 0; i < nums.length; i++) {
+        if (setArray.has(nums[i])) {
+            return true
+        } else {
+            setArray.add(nums[i])
+        }
+    }
+    return false
+}
+
+console.log('first variant', containsDuplicate(nums))
+
+
+/**
+ * Second variant
+ * Time O(N) | Space O(N)
+ */
+function containsDuplicateSecondVariant(nums) {
+    let setArray = new Set(nums)
+
+    if (setArray.size !== nums.length) return true
+
+    return false
+}
+
+console.log('second variant', containsDuplicateSecondVariant(nums))
